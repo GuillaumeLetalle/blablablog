5c9bbeb4987a725dfe180029544f2add7bdc8a89
guillaumeletalle <let.guillaume@gmail.com>
2023-08-01 16:59:01 +0200

    no authentifier WIP

---
+++ src/Controller/Front/FrontController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    #[Route('/front', name: 'app_front')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    #[Route('/new', name: 'front_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plaintextPassword = $user->getPassword();
+            $hasedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hasedPassword);
+            $user->setRoles(['ROLE_IDENTIFIED']);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_login', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('front/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+}
